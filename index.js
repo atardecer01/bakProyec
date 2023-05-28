4df267401a3477e64069279165ce9ea242cc9352
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-05-27 21:46:18 -0600

    ab

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 
-
 const app = express();
 
 app.use(express.json());
@@ -17,67 +16,47 @@ try {
 
     const url = `${db.connection.host}:${db.connection.port}`
     console.log(`MongoDB conectado en: ${url}`);
-
 } catch (error) {
     console.log(`error: ${error.message}`);
     process.exit(1);
 }
 
 const usuarioSchema = mongoose.Schema({
-    nombre:  String,
-       
+    nombre: String,
     password: String,
-        
-    email:  String,
-        
+    email: String,
 });
-
 
 const Usuario = mongoose.model("Usuarios", usuarioSchema);
 
 const autenticarMiddleware = async (req, res, next) => {
-    //const { email, password } = req.body;
-    console.log('pase por la funcion')
-    return res.status(200).json('ok');
-    /*
+    const { email, password } = req.body;
+
     try {
         // Comprobar si el usuario existe
         const usuario = await Usuario.findOne({ email });
-        console.log(usuario);
+
         if (!usuario) {
-            console.log('no esta el usuario')
-           // return res.status(401).json('Credenciales inválidas');
-           return res.status(200).json('ok');
+            return res.status(401).json('Credenciales inválidas');
         }
 
         // Revisar Password
         if (usuario.password === password) {
-            console.log('no hay contraseña')
-            return res.status(200).json('ok');
-            
+            return next(); // Llamar al siguiente middleware
         } else {
-            console.log('no funciona nooo')
-           return res.status(400).json('no');
-            
+            return res.status(400).json('Contraseña incorrecta');
         }
-
-        // Continuar con el siguiente middleware
-        next();
     } catch (error) {
         console.log(error);
-        console.log('erro grandeee')
         res.status(500).json('Error interno del servidor');
-    }*/
+    }
 };
 
-
-
+// Uso de la función middleware
+app.use('/api/login', autenticarMiddleware);
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
-
-// Uso de la función middleware
-app.use('/api/login', autenticarMiddleware);
