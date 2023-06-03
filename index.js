372785cbc364737d3f647079bda68247b867722f
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-06-03 12:27:02 -0600

    autenticar desde llamado

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import dotenv from 'dotenv';
-import mongoose from "mongoose";
 import cors from 'cors';
 import conectarDB from "./config/db.js";
 import Usuario from "./models/Usuario.js";
-
+import { autenticar } from "./controllers/usuarioController.js";
 const app = express();
 
 app.use(express.json());
@@ -14,35 +13,11 @@ dotenv.config();
 conectarDB();
 
 
-const autenticarMiddleware = async (req, res, next) => {
-    const { email, password } = req.body;
-
-    try {
-        // Comprobar si el usuario existe
-        const usuario = await Usuario.findOne({ email });
-
-        if (!usuario) {
-            return res.status(401).json('Credenciales inválidas');
-        }
-
-        // Revisar Password
-        if (usuario.password === password) {
-            req.usuario = usuario; // Agregar el objeto de usuario a la solicitud
-            next(); // Llamar al siguiente middleware
-        } else {
-            return res.status(400).json('Contraseña incorrecta');
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json('Error interno del servidor');
-    }
-};
-
 // Habilitar CORS
 app.use(cors());
 
 // Uso de la función middleware
-app.post('/api/login', autenticarMiddleware, (req, res) => {
+app.post('/api/login', autenticar, (req, res) => {
     // Se llega a este punto solo si la autenticación fue exitosa
     res.status(200).json('ok');
 });
